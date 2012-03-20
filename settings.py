e25234ce211c94a5be392f09d218d02798136870
Raullen <raullenchai@gmail.com>
2012-03-20 11:41:53 -0700

    a proof-of-concept version

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 #one need to input a weibo's account info (username & password) here
 #one need to get the app key for this account as well
 
-USERNAME = 'noreply@literhub.com'
-PASSWORD = '1224213qqq'
-APP_KEY = '1498737208'
+USERNAME = 'Alice'
+PASSWORD = '123456'
+APP_KEY = '1234567890'
 WEIBO_URL = 'https://api.weibo.com/2/friendships/followers.json?source='+APP_KEY+'&count=200'
 
 #do not add too many records per visit of the page, 500 records consume ~30% quota of Datastore Write Operations on my GAE
